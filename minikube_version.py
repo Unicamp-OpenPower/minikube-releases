8ffa13f78f10b38b8cc72791376738b643cb1b44
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-03-28 20:28:21 -0300

    Update minikube_version.py

--- minikube_version.py
+++ minikube_version.py
@@ -2,11 +2,11 @@ import requests
 
 # find and save the current Minikube release
 html = str(
-    requests.get('https://github.com/kubernetes/minikube/releases').content)
-index = html.find('tree/')
-current_version = html[index + 6:index + 12]
+    requests.get('https://github.com/kubernetes/minikube/releases/latest').content)
+index = html.find('Release ')
+current_version = html[index + 9:index + 15]
 file = open('current_version.txt', 'w')
-file.writelines(current_version)
+file.writelines(current_version.strip(" "))
 file.close()
 
 # find and save the current Minikube version on FTP server
@@ -14,15 +14,15 @@ html = str(
     requests.get(
         'https://oplab9.parqtec.unicamp.br/pub/ppc64el/minikube/'
     ).content)
-index = html.find('/ta')
-ftp_version = html[index - 145:index - 139]
+index = html.rfind('minikube_')
+ftp_version = html[index + 9:index + 15]
 file = open('ftp_version.txt', 'w')
-file.writelines(ftp_version)
+file.writelines(ftp_version.strip(" "))
 file.close()
 
-# find and save the last Minikube version on FTP server
-index = html.find('_')
-delete = html[index + 1:index + 7]
+# find and save the oldest Minikube version on FTP server
+index = html.find('minikube_')
+delete = html[index + 9:index + 15]
 file = open('delete_version.txt', 'w')
-file.writelines(delete)
+file.writelines(delete.strip(" "))
 file.close()
